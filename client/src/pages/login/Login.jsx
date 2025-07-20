33a47e4edfaf1c1b795d5c78fda7a765dcc8a6db
Aravelli Vishnu Priya <23A91A05E1@aec.edu.in>
2025-07-20 19:21:27 +0530

    [vishnu Priya- login page]

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,75 @@
+import React, { useState } from 'react';
 import './Login.css';
+import myImage from './Login-img1.jpeg';
+import { FaUser, FaEye, FaEyeSlash } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
-    return (
-        <section>
-            hi 
-            bro...
-        </section>
-    )
-}
+  const togglePassword = () => {
+    setShowPassword(prev => !prev);
+  };
 
-export default Login
-;
+  return (
+    <div className="Login">
+      <div className="LoginForm">
+        <form action="">
+          <h1>Login</h1>
+
+          {/* Username Input */}
+          <div className="Login-Input">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+              placeholder=" "
+            />
+            <label htmlFor="username">Username or Email</label>
+            <FaUser className="icon" />
+          </div>
+
+          {/* Password Input with toggle */}
+          <div className="Login-Input">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              required
+              placeholder=" "
+            />
+            <label htmlFor="password">Password</label>
+            <span onClick={togglePassword} style={{ cursor: 'pointer' }}>
+              {showPassword ? (
+                 <FaEye className="icon" />
+              ) : (
+              
+                 <FaEyeSlash className="icon" />
+              )}
+            </span>
+          </div>
+
+          {/* Remember Me & Forgot */}
+          <div className="Login-forget">
+            <label><input type="checkbox" /> Remember me</label>
+            <a href="#">Forgot Password?</a>
+          </div>
+
+          <button type="submit">Login</button>
+
+          {/* Register Link */}
+          <div className="register-link">
+            <p>Don't have an account? <Link to="./pages/register/Register.jsx">Register</Link></p>
+          </div>
+        </form>
+      </div>
+
+      <div className="Login-img">
+        <img src={myImage} alt="img1" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
