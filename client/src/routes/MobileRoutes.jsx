4bac49cfaca4af308d696a21864e131557f6a32d
Nikhilcharan-dev <chintufru143@gmail.com>
2025-07-23 22:50:26 +0530

    [Nikhil]: Re-Checking Code

--- client/src/routes/MobileRoutes.jsx
+++ client/src/routes/MobileRoutes.jsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "../pages/mobile/login/Login.jsx";
-import Register from "../pages/mobile/register/Register.jsx";
-import Google from "../services/Google/Google.jsx";
-
-const router = createBrowserRouter([
-    { path: '/', element: <Login /> },
-    { path: '/register', element: <Register /> },
-    { path:'/google', element: <Google /> },
-])
-
-const MobileRoutes = () => {
-
-    return (
-        <RouterProvider router={router} />
-    )
-}
-
-export default MobileRoutes;
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Login from "../pages/mobile/login/Login.jsx";
+// import Register from "../pages/mobile/register/Register.jsx";
+// import Google from "../services/Google/Google.jsx";
+//
+// const router = createBrowserRouter([
+//     { path: '/', element: <Login /> },
+//     { path: '/register', element: <Register /> },
+//     { path:'/google', element: <Google /> },
+// ])
+//
+// const MobileRoutes = () => {
+//
+//     return (
+//         <RouterProvider router={router} />
+//     )
+// }
+//
+// export default MobileRoutes;
