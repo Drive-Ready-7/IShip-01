69776b2d82e55a30a136ea3dd8d841389c9c3d22
nrokzzzz <23A91A0581@aec.edu.in>
2025-07-24 15:03:37 +0530

    [nrokzzzz] : routes handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { Suspense } from 'react';
 import DesktopRoutes from "./routes/DesktopRoutes.jsx";
 import MobileRoutes from './routes/MobileRoutes.jsx';
 import Loader from './components/loaders/simpleLoader/Loader.jsx';
+import useWindowSize from './routes/useWindoSize.jsx';
 
 import './App.css';
 
 const App = () => {
-
+    const width = useWindowSize();
     return (
         <Suspense fallback={ <Loader /> }>
-            <DesktopRoutes />
+            {/* routes ternary condtion */}
+            { width>768 ? <DesktopRoutes /> : <MobileRoutes/>}
         </Suspense>
     )
 }
