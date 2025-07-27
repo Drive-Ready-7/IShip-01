f722781e4b3270194fa50a080d94dd8737034486
nrokzzzz <23A91A0581@aec.edu.in>
2025-07-28 01:05:56 +0530

    [nrokzzzz]:navbar

--- client/src/pages/mobile/Navbar/navbar.jsx
+++ client/src/pages/mobile/Navbar/navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './navbar.css'
 import { useNavigate } from 'react-router-dom';
 
@@ -5,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 export default function Navbar(){
     const navigate = useNavigate()
     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    const [isOpen,setIsOpen] = useState(false);
     return(
         <nav className='M-nav'>
             <div className="logo">
@@ -12,7 +14,27 @@ export default function Navbar(){
             </div>  
             <div className="container">
                 {(isLoggedIn)?(
-                    <p className='menu-Burger'>&#9776;</p>
+                    <div>
+                    <p style={{cursor:"pointer"}} onClick={()=>setIsOpen(!isOpen)} className='menu-Burger'>
+                        { !isOpen?
+                       <span> &#9776;</span>:
+                        <span style={{fontSize:'40px',cursor:"pointer"}}> &times; </span>
+                        }
+                        </p>
+                        <div className={`menu-bar ${isOpen ? 'slide-in' : 'slide-out'}`}>
+                            <ul className="top-menu-bar">
+                                <li>Profile</li>
+                                <li>Jobs</li>
+                                <li>InternShips</li>
+                                <li>Hackathons</li>
+                                <li>Privacy Policy</li>
+                                <li>LogOut</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+
                 ):(
                     <button onClick={()=>navigate('/login')} className='M-login'>Login</button>
                 )
