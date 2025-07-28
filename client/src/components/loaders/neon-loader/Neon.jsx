7812b46c41ff28a3b58d1d3c0e5e98ab67af0ae6
nrokzzzz <23A91A0581@aec.edu.in>
2025-07-28 22:39:23 +0530

    [nagu] : neon loader

---
+++ client/src/components/loaders/neon-loader/Neon.jsx
@@ -0,0 +1,14 @@
+import './Neon.css'
+
+export default function Neon(){
+    return(
+        <div class="dot-loader">
+            <div class="dot d1"></div>
+            <div class="second">
+                <div class="dot d2"></div>
+                <div class="dot d3"></div>
+                <div class="dot d4"></div>
+            </div>
+        </div>
+    )
+}
